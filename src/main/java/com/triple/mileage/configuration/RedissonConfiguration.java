1b3b29927821610416cd1366854b02435a203716
ydh6226 <ydh6226@naver.com>
2022-07-02 19:28:48 +0900

    #20 chore: 레디슨 import

---
+++ src/main/java/com/triple/mileage/configuration/RedissonConfiguration.java
@@ -0,0 +1,34 @@
+package com.triple.mileage.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+public class RedissonConfiguration {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedissonClient redisClient() {
+        Config config = new Config();
+        config.setCodec(new StringCodec());
+        config.useSingleServer()
+                .setAddress(getAddress())
+                .setPassword(redisProperties.getPassword());
+
+        return Redisson.create(config);
+    }
+
+    private String getAddress() {
+        return String.format("redis://%s:%s", redisProperties.getHost(), redisProperties.getPort());
+    }
+}
